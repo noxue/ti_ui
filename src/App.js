6cab873fedec88e17189576b6f6b33547cea0461
noxue <173126019@qq.com>
2022-05-07 10:08:10 +0800

    v2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,212 @@
-import logo from './logo.svg';
+
+import { Layout, Card, Breadcrumb, Row, Col, Table } from 'antd';
+
+import 'antd/dist/antd.css';
 import './App.css';
+import axios from 'axios';
+import Excel from 'exceljs';
+import { useState, useEffect } from 'react';
+
+function decodeUnicode(str) {
+  return str.replace(/\\u([\d\w]{4})/gi, function (match, grp) {
+    return String.fromCharCode(parseInt(grp, 16));
+  });
+}
+
+const api_host = "http://ti.code.noxue.com"
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const product_columns = [
+  {
+    title: '型号',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '备注',
+    dataIndex: 'comment',
+    key: 'comment',
+    render: (text) => {
+      return decodeUnicode(text);
+    }
+  },
+  {
+    title: '当前库存',
+    dataIndex: 'count',
+    key: 'count',
+  },
+  {
+    title: '优先级',
+    dataIndex: 'rank',
+    key: 'rank',
+  },
+
+];
+
+
+const task_columns = [
+  {
+    title: '任务编号',
+    dataIndex: 'task_id',
+    key: 'name',
+  },
+  {
+    title: '型号',
+    dataIndex: ['product', 'name'],
+    key: 'product_name',
+  },
+  {
+    title: '任务创建时间',
+    dataIndex: 'created_at',
+    key: 'created_at',
+    render: (text, record) => {
+      // 时间戳转年月日时分秒
+      var date = new Date(text * 1000);
+      var Y = date.getFullYear() + '-';
+      var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+      var D = date.getDate() + ' ';
+      var h = date.getHours() + ':';
+      var m = date.getMinutes() + ':';
+      var s = date.getSeconds();
+      return Y + M + D + h + m + s;
+    }
+  },
+];
+
+// 点击表单按钮，获取上传文件内容
+async function getFile(e) {
+  var file = e.target.files[0];
+  var reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  reader.onload = function (e) {
+    var data = e.target.result;
+    var workbook = new Excel.Workbook();
+    workbook.xlsx.load(data).then(function () {
+      var worksheet = workbook.getWorksheet(1);
+      var rowCount = worksheet.rowCount;
+      var colCount = worksheet.columnCount;
+      var data = [];
+      for (var i = 1; i <= rowCount; i++) {
+        var row = [];
+        for (var j = 1; j <= colCount; j++) {
+          row.push(worksheet.getCell(i, j).value);
+        }
+        data.push(row);
+      }
+
+      // 排除第一行，生成json
+      var json = [];
+      for (var i = 1; i < data.length; i++) {
+        var obj = {};
+
+        // 中文转unicode
+        for (var j = 0; j < data[i].length; j++) {
+          if (data[i][j] != null) {
+            data[i][j] = (data[i][j] + "").replace(/[\u4e00-\u9fa5]/g, function (match) {
+              return '\\u' + match.charCodeAt(0).toString(16);
+            });
+
+          }
+        }
+        console.log(data[i][1]);
+        // 数字转字符串
+        var obj = { name: (data[i][0] + "").trim(), comment: data[i][1] };
+        json.push(obj);
+      }
+      console.log(json);
+      console.log(data);
+
+      // axios post json
+      axios.post(api_host + '/products', json)
+        .then(function (response) {
+          console.log(response);
+          alert("上传成功");
+          // 刷新页面
+          window.location.reload();
+        })
+        .catch(function (error) {
+          alert("提交出错，请重新提交:" + error)
+          window.location.reload();
+          console.log(error);
+        });
+
+    });
+  }
+}
+
 
 function App() {
+
+  // 创建state
+  const [products, setProducts] = useState([]);
+  // tasks state
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      axios.get(api_host + '/products')
+        .then(function (response) {
+          console.log(response);
+          setProducts(response.data.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+
+      axios.get(api_host + '/tasks')
+        .then(function (response) {
+          console.log(response);
+          setTasks(response.data.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }, 2000);
+    return () => clearInterval(timer);
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>芯片</Breadcrumb.Item>
+          <Breadcrumb.Item>库存</Breadcrumb.Item>
+          <Breadcrumb.Item>监控</Breadcrumb.Item>
+        </Breadcrumb>
+        {/* 获取上传文件内容 */}
+        <Row>
+          <Col span={24}>
+            <div>
+              设置要查询的产品：<input type="file" id="file" onChange={getFile} />
+            </div>
+          </Col>
+        </Row>
+
+
+        <Row>
+          <Col span={12}>
+
+            <Card title="待查询的产品队列" bordered={false} style={{ margin: 10 }}>
+              <Table columns={product_columns} dataSource={products} pagination={false} size="small" />
+            </Card>
+          </Col>
+
+          <Col span={12}>
+            <Card title="正在查询的产品列表" bordered={false} style={{ margin: 10 }}>
+              <Table columns={task_columns} dataSource={tasks} pagination={false} size="small" />
+
+
+            </Card>
+          </Col>
+        </Row>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+
   );
 }
 
